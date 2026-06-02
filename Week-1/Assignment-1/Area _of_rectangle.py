12230002f83263ef44207fb2e3dc4a3b6334f50e
SADHIKA RANA <ranasadhikaa@gmail.com>
2026-06-02 17:10:41 +0530

    Add files via upload

---
+++ Week-1/Assignment-1/Area _of_rectangle.py
@@ -0,0 +1,5 @@
+# 1. Find Area of rectangle 
+length = float(input("Enter the length of the rectangle:"))
+width = float(input("Enter the width of the rectangle:"))
+Area_of_rectangle = length * width
+print("Area of rectangle is:", Area_of_rectangle)
